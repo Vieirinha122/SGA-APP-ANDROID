e71f4ac80786a0f6be7cf6050891ab39bb08c37e
Arthur-Vieira-Sociiz <arthurvieira@sociiz.com.br>
2026-07-10 12:12:49 -0300

    carregando url com token fixo para testar.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import AppSettings from "../types/types";
 export const BASE_URL = "https://sga-sociiz.netlify.app";
 
 export const DEFAULT_SETTINGS: AppSettings = {
-  painelToken: "",
+  painelToken: "FA2D614559D2581D4C532270006BB026",
   kiosk: true,
   lastPage: "painel",
 };
